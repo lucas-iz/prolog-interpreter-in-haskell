0d126b580555f5391df72fd10e7f7aa1c994cbf9
Bastian Oertel <oertel.ba@live.de>
2022-03-02 11:57:02 +0100

    Term

--- aufgabe3.hs
+++ aufgabe3.hs
@@ -5,5 +5,13 @@ class Vars a where
 
 instance Vars Term where
     allVars (Var (VarName x)) = [VarName x]
-    allVars (Comb x []) = []
+    allVars (Comb _ []) = []
+    allVars (Comb _ t) = allVarsH t
+     where
+        allVarsH [] = [] 
+        allVarsH (Var (VarName x) : xs) =  [VarName x] ++ allVarsH xs
+        allVarsH ((Comb _ []) : xs) = allVarsH xs
+        allVarsH ((Comb _ t1) : xs) = allVarsH t1 ++ allVarsH xs
+
+
 
