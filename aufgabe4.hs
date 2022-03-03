f55930d46a9b962fad6cfde5d791c29a5140fc2d
Lucas Hahn <lhahn98@gmail.com>
2022-03-03 08:43:51 +0100

    Modifikation vom Datentyp Subst. Anstatt eines Tupels eine Liste an Tupeln. Anpassung in den anderen Funktionen besonders in 'domain'.

--- aufgabe4.hs
+++ aufgabe4.hs
@@ -2,25 +2,26 @@
 import Type
 import Aufgabe3
 
-data Subst = Single (VarName, Term) | Empty
+data Subst = Subst [(VarName, Term)] | Empty
   deriving (Show)
 
 -- {A->B} // (a,b)
 
 domain :: Subst -> [VarName]
-domain (Single (a, b)) = filter (/= a) (allVars b)
+--domain (Subst (a, b)) = filter (/= a) (allVars b)
+domain (Subst ((a, b) : rest)) = filter (/= a) (allVars b) ++ domain (Subst rest)
 
--- Aufruf: domain(Single (VarName "c", Comb "1" [Var (VarName "b"), Var (VarName "c")]))
+-- Aufruf: domain(Subst (VarName "c", Comb "1" [Var (VarName "b"), Var (VarName "c")]))
 
 empty :: Subst
 empty = Empty
 
 single :: VarName -> Term -> Subst
-single a b = Single (a, b)
+single a b = Subst [(a, b)]
 
 -- apply :: Subst -> Term -> Term
 -- apply Empty t = t
--- apply (Single (a,b)) t =
+-- apply (Subst (a,b)) t =
 
 -- Var VarName
 -- Comb CombName [Term]
