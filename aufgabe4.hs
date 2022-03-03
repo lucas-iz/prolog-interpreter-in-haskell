36d9f642cc83b1f9386c54e660a90b4e9707ab50
Lucas Hahn <lhahn98@gmail.com>
2022-03-03 15:10:19 +0100

    apply Vars

--- aufgabe4.hs
+++ aufgabe4.hs
@@ -1,33 +1,40 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 import Type
 import Aufgabe3
 
+-- {A->B} // (a,b)
 data Subst = Subst [(VarName, Term)] | Empty
   deriving (Show)
 
--- {A->B} // (a,b)
-
+-- Aufruf: domain(Subst (VarName "c", Comb "1" [Var (VarName "b"), Var (VarName "c")]))
 domain :: Subst -> [VarName]
 --domain (Subst (a, b)) = filter (/= a) (allVars b)
 domain Empty = []
 domain (Subst []) = []
 domain (Subst ((a, b) : rest)) = filter (/= a) (allVars b) ++ domain (Subst rest)
 
--- Aufruf: domain(Subst (VarName "c", Comb "1" [Var (VarName "b"), Var (VarName "c")]))
-
 empty :: Subst
 empty = Empty
 
 single :: VarName -> Term -> Subst
-single a b = Subst [(a, b)]
+single a b | a `elem` (allVars b) = Empty          -- verhindert {A->A}
+           | otherwise            = Subst [(a, b)]
 
 apply :: Subst -> Term -> Term
-apply Empty t = t
+apply Empty t      = t
 apply (Subst []) t = t
+apply (Subst [(a,b)]) (Var t) | a == t    = b
+                              | otherwise = Var t
+apply (Subst (r:rs)) (Var t)  = apply (Subst rs) (apply (Subst [r]) (Var t))
+-- apply (Subst [(a,b)]) (Comb n (t:ts)) = apply (Subst [(a,b)]) (Comb n (ersetze:ts))
+--    where 
+--       ersetze = apply (Subst [(a,b)]) t)                               
 
--- apply (Subst (x:xs)) (Var name) = ???
+
+-- apply (Subst [(VarName "I", Var (VarName "J")),(VarName "J", Var (VarName "H"))]) (Var (VarName "I"))
+
 -- apply (Subst (x:xs)) (Comb _ list) = ???
 
--- [(a,b),(c,d),(e,f)] (Var (VarName "A"))
 
 -- Var VarName
 -- Comb CombName [Term]
