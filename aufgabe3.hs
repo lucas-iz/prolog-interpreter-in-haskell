34d36bfccd42259172b1b9e84d2d9ff7a566b024
Bastian Oertel <oertel.ba@live.de>
2022-03-06 15:27:45 +0100

    changes complete

--- aufgabe3.hs
+++ aufgabe3.hs
@@ -1,54 +1,34 @@
 module Aufgabe3 where
 import Type
+import Data.List
 
 class Vars a where
     allVars :: a -> [VarName]
 
+-- change complete (concat und map) 
+-- change complete (using imported nub)
 instance Vars Term where
     allVars (Var (VarName x)) = [VarName x]
     allVars (Comb _ []) = []
-    allVars (Comb _ t) =  nubV (allVarsH t)
-     where
-        allVarsH [] = []
-        allVarsH (Var (VarName x) : xs) = VarName x : allVarsH xs
-        allVarsH ((Comb _ []) : xs) = allVarsH xs
-        allVarsH ((Comb _ t1) : xs) = allVarsH t1 ++ allVarsH xs
+    allVars (Comb _ t) =  nub (concatMap allVars t)
+  
 instance Vars Rule where
   allVars (Rule t []) = allVars t
-  allVars (Rule t ts) = nubV (allVars t ++ sub ts)
-    where
-      sub [] = []
-      sub (x : xs) = allVars x ++ sub xs
+  allVars (Rule t ts) = nub (allVars t ++ concatMap allVars ts)
+   
 instance Vars Prog where
   allVars (Prog []) = []
-  allVars (Prog (p : ps)) = nubV (allVars p ++ allVars (Prog ps))
+  allVars (Prog (p : ps)) = nub (allVars p ++ allVars (Prog ps))
+
 instance Vars Goal where
   allVars (Goal []) = []
-  allVars (Goal (t : ts)) = nubV (allVars t ++ allVars (Goal ts))
+  allVars (Goal (t : ts)) = nub (allVars t ++ allVars (Goal ts))
 
--- Remove all duplicates in a list of VarNames.
-nubV :: [VarName] -> [VarName]
-nubV []     = []
-nubV (x:xs) = x : nubV (filter (/=x) xs)
+
+
 
 -- gives an infinite list of valid variable names
+-- change complete (using map)
 freshVars :: [VarName]
-freshVars = charToVarName capitalLetters ++ tupelToVarName (crossproduct [0 ..] ['A' .. 'Z'])
+freshVars = map (\x -> VarName [x]) ['A' .. 'Z'] ++ map (\(x,y) -> VarName (y : show x)) ([(a, b) | a <- [0 ..], b <- ['A' .. 'Z']])
 
--- list of A - Z
-capitalLetters :: [Char]
-capitalLetters = ['A' .. 'Z']
-
--- forms a Char into a VarName
-charToVarName :: [Char] -> [VarName]
-charToVarName [] = []
-charToVarName (x : xs) = (VarName [x] : charToVarName xs)
-
--- forms a tupel into a VarName
-tupelToVarName :: [(Int, Char)] -> [VarName]
-tupelToVarName [] = []
-tupelToVarName ((x1,x2) : xs) = VarName (x2 : show x1) : tupelToVarName xs
-
--- computes cross product of 2 lists
-crossproduct :: [a] -> [b] -> [(a, b)]
-crossproduct listeA listeB = [(a, b) | a <- listeA, b <- listeB]
