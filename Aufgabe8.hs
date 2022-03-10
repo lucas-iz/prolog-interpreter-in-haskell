f21ef9d471ac9520bc922e1830b279cb36c44a66
Lucas Hahn <lhahn98@gmail.com>
2022-03-10 17:32:04 +0100

    keine AHnung was ich hier mache

--- Aufgabe8.hs
+++ Aufgabe8.hs
@@ -1,24 +1,24 @@
--- import Parser
+import Parser
 import Type
 
 main :: IO ()
 main = do  
     putStrLn "Welcome!"
     putStrLn "Type \":h\" for help." 
-    loop (Prog [])
+    loop (Prog []) (Goal []) "dfs"
 
-loop :: Prog -> IO ()
-loop p = do
+loop :: Prog -> Goal -> String -> String -> IO ()
+loop p g s f = do
    -- program <- p
    putStr "?- "
    input <- getLine
-   if length (words input) == 2 then cases p (head (words input)) (last (words input))
-                        else if length (words input) == 1 then cases p (head (words input)) ""
-                                                  else putStrLn "Sorry, Too many arguments. Try a different command" >> loop p
+   if length (words input) == 2 then cases p g s f (head (words input)) (last (words input))
+                        else if length (words input) == 1 then cases p g s f (head (words input)) ""
+                                                  else putStrLn "Sorry, Too many arguments. Try a different command" >> loop p g s f
 
-cases :: Prog -> String -> String -> IO ()
--- cases _ (Goal g) _    = do print (parse g)
-cases p ":h" _ = do
+cases :: Prog -> Goal -> String -> String -> String -> IO ()
+cases p g s f goal _ = if parse goal == (Goal _) then putStrLn (solveWith p goal s) else loop p g s f
+cases p g s f ":h" _ = do
    putStrLn "Commands available from the prompt:"
    putStrLn "   <goal>      Solves/proves the specified goal."
    putStrLn "   :h          Shows this help message."
@@ -30,23 +30,27 @@ cases p ":h" _ = do
    putStrLn "               where <strat> is one of 'dfs', 'bfs', or 'iddfs'."
    putStrLn "   :t <goal>   Prints the SLD tree for the specified goal."
    loop p
-cases _ ":l" _ = return ()
-cases _ ":p" _ = return ()
-cases _ ":q" _ = return ()
-cases _ ":r" _ = return ()
-cases p ":s" arg 
+cases p g s f ":l" file = do
+                  putStrLn "New File loaded."
+                  loop (parse file) g s file
+cases p g s f ":p" _ = putStrLn (pretty p)
+cases p g s f ":q" _ = return ()
+cases p g s f ":r" _ = do
+                  putStrLn "File reloaded."
+                  loop (parse f) g s f
+cases p g s f ":s" arg 
             | arg == "bfs" = do 
                   putStrLn "Strategy set to 'breadth-first search'."
                   -- Change strategy
-                  loop p
+                  loop p g "bfs"
             | arg == "dfs" = do 
                   putStrLn "Strategy set to 'depth-first search'."
                   -- Change strategy
-                  loop p
+                  loop p g "dfs"
             | arg == "iddfs" = do 
                   putStrLn "Strategy set to 'iterative deepening depth-first search'."
                   -- Change strategy
-                  loop p
-            | otherwise = putStrLn "Sorry, strategy not found. Try a different command" >> loop p
-cases _ ":t" _ = return ()
-cases p _ _ = loop p
+                  loop p g "iddfs"
+            | otherwise = putStrLn "Sorry, strategy not found. Try a different command" >> loop p g s f
+cases p g s f ":t" goal = putStrLn (pretty (sld p goal))
+cases p g s f _ _ = loop p g s f
